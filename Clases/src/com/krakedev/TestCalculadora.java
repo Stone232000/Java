826e9645cac2b9657672d8ae69a74c44dda3ddf5
Stone232000 <emiliopiedra2000@gmail.com>
2023-12-11 15:06:59 -0500

    creada la funcion dividir

--- Clases/src/com/krakedev/TestCalculadora.java
+++ Clases/src/com/krakedev/TestCalculadora.java
@@ -6,13 +6,19 @@ int x=5;
 		// TODO Auto-generated method stub
 		int resultadosum;
 		int resultadores;
+		double resultadomul;
+		double resultadodiv;
 		Calculadora c1=new Calculadora();
 		 resultadosum=c1.Sumar(5, 5);
 		 resultadores=c1.Restar(10, 5);
+		 resultadomul=c1.multiplicar(2.0, 3.0);
+		 resultadodiv=c1.dividir(10, 5);
 		 System.out.println(resultadosum);
 		System.out.println(resultadores);
-		TestCalculadora myObj=new TestCalculadora();
-		System.out.println(myObj.x);
+		System.out.println(resultadomul);
+		System.out.println(resultadodiv);
+		
+	
 
 	}
 

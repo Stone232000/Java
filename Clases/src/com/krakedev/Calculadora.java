ff8d9f829a727e1b709124d4942185bcd4b40ffe
Stone232000 <emiliopiedra2000@gmail.com>
2023-12-11 15:14:32 -0500

    creada la funcion promedio

--- Clases/src/com/krakedev/Calculadora.java
+++ Clases/src/com/krakedev/Calculadora.java
@@ -21,9 +21,17 @@ public class Calculadora {
 		return resultado;
 
 	}
+
 	public double dividir(double valor1, double valor2) {
 		double resultado;
 		resultado = valor1 / valor2;
+		return resultado;
+
+	}
+
+	public double promediar(double valor1, double valor2, double valor3) {
+		double resultado;
+		resultado = ((valor1 + valor2 + valor3)/3);
 		return resultado;
 
 	}
--- Clases/src/com/krakedev/TestCalculadora.java
+++ Clases/src/com/krakedev/TestCalculadora.java
@@ -8,15 +8,18 @@ int x=5;
 		int resultadores;
 		double resultadomul;
 		double resultadodiv;
+		double resultadoprom;
 		Calculadora c1=new Calculadora();
 		 resultadosum=c1.Sumar(5, 5);
 		 resultadores=c1.Restar(10, 5);
 		 resultadomul=c1.multiplicar(2.0, 3.0);
 		 resultadodiv=c1.dividir(10, 5);
+		 resultadoprom=c1.promediar(15, 10,10);
 		 System.out.println(resultadosum);
 		System.out.println(resultadores);
 		System.out.println(resultadomul);
 		System.out.println(resultadodiv);
+		System.out.println(resultadoprom);
 		
 	
 
